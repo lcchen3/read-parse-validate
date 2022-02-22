6926b1b99febddcdc951d7e0b3d841e4dd565eb6
lcchen3 <lcchen3@mit.edu>
2022-02-22 10:42:53 -0500

    lecture

--- 01_read_json/read_json.py
+++ 01_read_json/read_json.py
@@ -1,29 +1,30 @@
 # ---------------------------------------------
 # import package to read json
 # ---------------------------------------------
-
+import json
 
 # ---------------------------------------------
 #  write function to load file and parse json
 # ---------------------------------------------
-
 def readJson(file):
-    pass
+    with open(file) as p:
+        return json.load(p) 
 
 # ---------------------------------------------
 #  call 'readJson', load salaries
 # ---------------------------------------------
-
+salaries = readJson('./data.json')
 
 # ---------------------------------------------
 #  print all salaries
 # ---------------------------------------------
-
+print(salaries)
 
 # ---------------------------------------------
 # loop through list, only print salary field
 # ---------------------------------------------
-
+for salary in salaries['data']:
+   print(float(salary[18]))
 
 # ---------------------------------------------
 #  add all salaries
