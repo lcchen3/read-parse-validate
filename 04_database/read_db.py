c30d028ab6759da3afc36ff0db618b445de43ba8
lcchen3 <lcchen3@mit.edu>
2022-02-23 10:46:34 -0500

    lecture finish commit

--- 04_database/read_db.py
+++ 04_database/read_db.py
@@ -4,17 +4,18 @@ import mysql.connector
 cnx = mysql.connector.connect(user='root',
     password='Geogoog74!', #use user access from SQL workbench
     host='127.0.0.1', #own machine
-    database='sys',
+    database='Farm',
     auth_plugin='mysql_native_password')
 
 cursor = cnx.cursor()
-query = ("SHOW DATABASES")
+query = ("INSERT INTO `Customers` VALUES ('kara','danvers','kara@mit.edu')")
 cursor.execute(query)
 
-#loop through data and print to terminal
+#loop through data and print to terminal for SELECT 
 for row in cursor.fetchall():
     print(row)
 
 #clean up
+cnx.commit()
 cursor.close()
 cnx.close() 
